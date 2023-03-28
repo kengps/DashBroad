23fc94fbe81b770d3b81a3bf448d0a17da952a54
DESKTOP-O0R044J\PC2 <kengzer5@hotmail.com>
2023-03-28 16:00:42 +0700

    add menu

--- client/src/components/SideMenu/Index.jsx
+++ client/src/components/SideMenu/Index.jsx
@@ -1,36 +1,61 @@
 import { Menu } from 'antd'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import {HomeOutlined , DashboardOutlined , FormOutlined ,UnorderedListOutlined ,LoadingOutlined ,UserOutlined ,EditOutlined ,TeamOutlined} from '@ant-design/icons'
 const SideMenu = () => {
   const navigate = useNavigate()
   return (
     <div className="SideMenu">
       <Menu
+      mode='inline'
         onClick={(item) => {
           //item.key
           navigate(item.key);
         }}
         items={[
           {
-            label: "DashBorad",
+            label: "Dashborad",
             key: "/",
+            icon: <DashboardOutlined />
           },
           {
-            label: "ฟอร์มบันทึกเคส",
-            key: "/formcontrol",
+            label: "หน้าการทำงาน",
+            key: "homework", 
+            icon: <HomeOutlined /> ,
+            children:[{
+              label: "ฟอร์มบันทึกเคส",
+              key: "/formcontrol",
+              icon: <FormOutlined />
+            },
+            {
+              label: "รายการเคสทั้งหมด",
+              key: "/listcase",
+              icon: <UnorderedListOutlined />
+            },
+            {
+              label: "รายการเคสที่ยังไม่ได้แก้ไข",
+              key: "/listunresolve",
+              icon:<LoadingOutlined />
+            },
+            ]
           },
-          {
-            label: "รายการเคสทั้งหมด",
-            key: "/listcase",
-          },
-          {
-            label: "รายการเคสที่ยังไม่ได้แก้ไข",
-            key: "/listunresolve",
-          },
+         
           {
             label: "Customers",
-            key: "/listuser",
+            key: "user",
+            icon:<UserOutlined />,
+              children:[
+                {
+                  label: "แก้ไขรหัสผ่าน",
+                  key: "/listuser",
+                  icon:<EditOutlined />
+                },
+                {
+                  label: "สมาชิกทั้งหมด",
+                  key: "/listuser",
+                  icon:<TeamOutlined />
+                }
+              ]
           },
         ]}
       ></Menu>
