9a6d215afb2a052dfe7926d33412863b9c8f2a55
kengps <106742004+kengps@users.noreply.github.com>
2023-12-21 20:28:36 +0700

    add

--- server/services/cron_sendCase.js
+++ server/services/cron_sendCase.js
@@ -3,33 +3,33 @@ const axios = require('axios');
 const Cases = require('../models/caseModel')
 const moment = require("moment");
 
+const sendTelegramMessage = async (text) => {
+    try {
+        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+            chat_id: `${process.env.TELEGRAM_CHATID_GROUB}`,
+            text: text,
+        });
+    } catch (error) {
+        console.error('Error sending message:', error.message);
+    }
+};
 
-const cronSendCase = new cron('30 8,20 * * *', async () => {
-    const caseAwait = await Cases.find();
-    const data = caseAwait.filter((item) => { return item.status === "รอการแก้ไข" })
-
-    const filteredData = caseAwait
-        .filter((item) => item.status === "รอการแก้ไข")
-        .map((item) => item.caseId);
-
-    const currentTime1 = moment();
+const generateSummaryMessage = (data, currentTime1) => {
     const isMorning = currentTime1.isBetween(moment('09:35', 'HH:mm'), moment('20:35', 'HH:mm'));
     const timeOfDay = isMorning ? '(กะเช้า 🌞)' : '(กะดึก 🌛)';
 
-    // // ลบวันที่ 1 วัน ถ้าเป็นกะดึก
+    // ลบวันที่ 1 วัน ถ้าเป็นกะดึก
     if (!isMorning) {
         currentTime1.subtract(1, 'day');
     }
 
     const formattedDate = currentTime1.locale('th').format('lll');
 
-
-    let msg = ""
+    let msg = "";
     if (data.length === 0) {
         msg = ` สรุปการทำงานประจำวันที่ ${formattedDate} ${timeOfDay} \n\n`;
-        msg += "- ไม่มีรายการค้าง"
+        msg += "- ไม่มีรายการค้าง";
     } else {
-
         msg = ` สรุปการทำงานประจำวันที่ ${formattedDate}  ${timeOfDay}\n\n`;
         msg += `เคสค้างจำนวน ${data.length} รายการ\n\n`;
 
@@ -38,16 +38,16 @@ const cronSendCase = new cron('30 8,20 * * *', async () => {
         });
     }
 
-    try {
-        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
-            chat_id: `${process.env.TELEGRAM_CHATID_GROUB}`,
-            text: msg,
-        });
+    return msg;
+};
 
-        // console.log(response.data);
-    } catch (error) {
-        console.error('Error sending message:', error.message);
-    }
+const cronSendCase = new cron('30 8,20 * * *', async () => {
+    const caseAwait = await Cases.find();
+    const data = caseAwait.filter((item) => { return item.status === "รอการแก้ไข" });
+    
+    const msg = generateSummaryMessage(data, moment());
+
+    await sendTelegramMessage(msg);
 });
 
 module.exports = cronSendCase;
